2a6d09ede0e2ed476c6e58a95f1e04e8bd740bd7
Debbie <debbiecamera0704@gmail.com>
2021-06-14 22:07:59 +0800

    feat: initialize project

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+
+app.use('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //handlebars註冊樣板引擎
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
