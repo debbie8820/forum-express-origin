f85bdc9bb469c1818bc558c37bd20ce07ff7e860
Debbie <debbiecamera0704@gmail.com>
2021-06-15 17:20:43 +0800

    install express-session & connect-flash and verification on signup page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models') //引入資料庫
 const app = express()
 const port = 3000
@@ -8,6 +10,13 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //handlebars註冊樣板引擎
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: 'false', saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.err_msg = req.flash('err_msg')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,10 +9,20 @@ const userController = {
 
   signUp: (req, res) => {
     const { name, email, password, confirmPassword } = req.body
-    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-    User
-      .create({ name, email, password: hashPassword })
-      .then(() => res.redirect('/signin'))
+    if (password !== confirmPassword) {
+      req.flash('err_msg', '密碼與確認密碼不符')
+      return res.redirect('/signup')
+    }
+    User.findOne({ where: { email } }).then((user) => {
+      if (user) {
+        req.flash('err_msg', '此信箱已註冊')
+        return res.redirect('/signup')
+      }
+      const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+      return User
+        .create({ name, email, password: hashPassword })
+        .then(() => res.redirect('/signin'))
+    })
   }
 }
 
