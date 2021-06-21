582e5997701735582823c776119189b64c167c97
Debbie <debbiecamera0704@gmail.com>
2021-06-21 18:31:03 +0800

    feat: add categories selector to views

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -18,7 +18,8 @@ const adminController = {
   },
 
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll({ raw: true, nest: true })
+      .then((categories) => { return res.render('admin/create', { categories }) })
   },
 
   postRestaurant: (req, res) => {
@@ -57,8 +58,11 @@ const adminController = {
   },
 
   editRestaurant: (req, res) => {
-    Restaurant.findByPk(req.params.id, { raw: true })
-      .then((restaurant) => { return res.render('admin/create', { restaurant }) })
+    Category.findAll({ raw: true, nest: true })
+      .then(categories => {
+        return Restaurant.findByPk(req.params.id)
+          .then((restaurant) => { return res.render('admin/create', { restaurant: restaurant.toJSON(), categories }) })
+      })
       .catch(err => { return res.status(422).json(err) })
   },
 
