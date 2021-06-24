0fafd5bca504cdc4ce1993abcca8e4fea86ec970
Debbie <debbiecamera0704@gmail.com>
2021-06-24 19:13:42 +0800

    add error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,25 +7,27 @@ const userController = {
     return res.render('signup')
   },
 
-  signUp: (req, res) => {
+  signUp: (req, res, next) => {
     const { name, email, password, confirmPassword } = req.body
     if (password !== confirmPassword) {
       req.flash('err_msg', '密碼與確認密碼不符')
       return res.redirect('/signup')
     }
-    User.findOne({ where: { email } }).then((user) => {
-      if (user) {
-        req.flash('err_msg', '此信箱已註冊')
-        return res.redirect('/signup')
-      }
-      const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-      return User
-        .create({ name, email, password: hashPassword })
-        .then(() => {
-          req.flash('success_msg', '你已成功註冊')
-          res.redirect('/signin')
-        })
-    })
+    User.findOne({ where: { email } })
+      .then((user) => {
+        if (user) {
+          req.flash('err_msg', '此信箱已註冊')
+          return res.redirect('/signup')
+        }
+        const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+        return User
+          .create({ name, email, password: hashPassword })
+          .then(() => {
+            req.flash('success_msg', '你已成功註冊')
+            res.redirect('/signin')
+          })
+      })
+      .catch(err => next(err))
   },
 
   signInPage: (req, res) => {
@@ -43,5 +45,4 @@ const userController = {
     return res.redirect('/signin')
   }
 }
-
 module.exports = userController
